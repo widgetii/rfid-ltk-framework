b0f9ac177eb0a3f02eb9712a784088ab5b462f17
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2013-08-27 12:29:53 +0700

    Core: Better name `RfReaderContext` read result reporting method.

--- ru.aplix.ltk.core/src/ru/aplix/ltk/core/reader/RfReaderContext.java
+++ ru.aplix.ltk.core/src/ru/aplix/ltk/core/reader/RfReaderContext.java
@@ -1,6 +1,14 @@
 package ru.aplix.ltk.core.reader;
 
 
+/**
+ * RFID reader context.
+ *
+ * <p>An instance of this class is passed to {@link RfReaderDriver#initRfReader(
+ * RfReaderContext) reader}, and can be used by this reader to report reader
+ * events. The messages sent though this context will be reported to all
+ * subscribed consumers.</p>
+ */
 public final class RfReaderContext {
 
 	private final RfReader reader;
@@ -9,12 +17,22 @@ public final class RfReaderContext {
 		this.reader = reader;
 	}
 
+	/**
+	 * Reports an RFID reader status update.
+	 *
+	 * @param statusMessage status update to report.
+	 */
 	public void updateStatus(RfReaderStatusMessage statusMessage) {
 		this.reader.readerSubscriptions().sendMessage(statusMessage);
 	}
 
-	public void sendTag(RfReadMessage tagMessage) {
-		this.reader.tagSubscriptions().sendMessage(tagMessage);
+	/**
+	 * Sends an RFID read result.
+	 *
+	 * @param readMessage read result to report.
+	 */
+	public void sendRead(RfReadMessage readMessage) {
+		this.reader.tagSubscriptions().sendMessage(readMessage);
 	}
 
 }

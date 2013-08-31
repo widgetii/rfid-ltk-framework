e2eae7bf19947af33f673cce435b57ca909ecc85
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2013-08-31 14:40:37 +0700

    Ctg: Register provider.

--- ru.aplix.ltk.driver.ctg/src/ru/aplix/ltk/driver/ctg/impl/CtgActivator.java
+++ ru.aplix.ltk.driver.ctg/src/ru/aplix/ltk/driver/ctg/impl/CtgActivator.java
@@ -2,16 +2,33 @@ package ru.aplix.ltk.driver.ctg.impl;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+import ru.aplix.ltk.core.RfProvider;
+import ru.aplix.ltk.driver.ctg.CtgRfProvider;
 
 
 public class CtgActivator implements BundleActivator {
 
+	private ServiceRegistration<?> providerRegistration;
+
 	@Override
 	public void start(BundleContext context) throws Exception {
+		this.providerRegistration = context.registerService(
+				new String[] {
+					RfProvider.class.getName(),
+					CtgRfProvider.class.getName(),
+				},
+				new CtgRfProviderImpl(),
+				null);
 	}
 
 	@Override
 	public void stop(BundleContext context) throws Exception {
+		if (this.providerRegistration != null) {
+			this.providerRegistration.unregister();
+			this.providerRegistration = null;
+		}
 	}
 
 }

ac3a5aba45410fe9e15ab97ae51e222a22633460
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2013-09-05 19:43:54 +0700

    Collector HTTP: No longer depends on servlet API.

--- ru.aplix.ltk.collector.http.jetty/src/ru/aplix/ltk/collector/jetty/Activator.java
+++ ru.aplix.ltk.collector.http.jetty/src/ru/aplix/ltk/collector/jetty/Activator.java
@@ -1,36 +1,78 @@
 package ru.aplix.ltk.collector.jetty;
 
+import static org.eclipse.equinox.http.jetty.JettyConfigurator.startServer;
+import static org.eclipse.equinox.http.jetty.JettyConfigurator.stopServer;
+import static ru.aplix.ltk.collector.http.CollectorHttpConstants.COLLECTOR_HTTP_SERVICE_ID;
+
+import java.util.Hashtable;
+
+import org.eclipse.equinox.http.jetty.JettyConstants;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 
 public class Activator implements BundleActivator {
 
-	private BundleContext context;
-	private CollectorJettyService jettyService;
+	private static final String PREFIX =
+			Activator.class.getPackage().getName() + '.';
+	private static final String PORT = PREFIX + "port";
+	private static final String CONTEXT_PATH = PREFIX + "context.path";
 
-	public final BundleContext getContext() {
-		return this.context;
-	}
+	private static final String DEFAULT_CONTEXT_PATH = "/collector";
+
 
 	@Override
 	public void start(BundleContext context) throws Exception {
-		this.context = context;
-
-		final CollectorJettyService jettyService = new CollectorJettyService();
-
-		jettyService.register(context);
-		this.jettyService = jettyService;
+		startServer(COLLECTOR_HTTP_SERVICE_ID, settings(context));
 	}
 
 	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
-		try {
-			this.jettyService.unregister();
-		} finally {
-			this.jettyService = null;
-			this.context = null;
+		stopServer(COLLECTOR_HTTP_SERVICE_ID);
+	}
+
+	private static Hashtable<String, Object> settings(BundleContext context) {
+
+		final Hashtable<String, Object> settings = new Hashtable<>();
+
+		setPort(context, settings);
+		setContextPath(context, settings);
+
+		return settings;
+	}
+
+	private static void setContextPath(
+			BundleContext context,
+			Hashtable<String, Object> settings) {
+
+		String contextPath = context.getProperty(CONTEXT_PATH);
+
+		if (contextPath == null) {
+			contextPath = DEFAULT_CONTEXT_PATH;
 		}
+
+		settings.put(JettyConstants.CONTEXT_PATH, contextPath);
+	}
+
+	private static void setPort(
+			BundleContext context,
+			Hashtable<String, Object> settings) {
+
+		final String port = context.getProperty(PORT);
+
+		if (port == null) {
+			return;
+		}
+
+		final Integer portNum;
+
+		try {
+			portNum = Integer.valueOf(port);
+		} catch (NumberFormatException e) {
+			return;
+		}
+
+		settings.put(JettyConstants.HTTP_PORT, portNum);
 	}
 
 }
---
+++ ru.aplix.ltk.collector.http/src/ru/aplix/ltk/collector/http/CollectorHttpConstants.java
@@ -0,0 +1,15 @@
+package ru.aplix.ltk.collector.http;
+
+
+/**
+ * Collector HTTP service constants.
+ */
+public interface CollectorHttpConstants {
+
+	/**
+	 * The identifier of collector {@code HttpService}.
+	 */
+	String COLLECTOR_HTTP_SERVICE_ID =
+			CollectorHttpConstants.class.getPackage().getName();
+
+}

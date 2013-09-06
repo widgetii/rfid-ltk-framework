b53f38da329cdb18ddc83bb00c4f828b503549be
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2013-09-06 15:46:47 +0700

    OSGi: Rename `OSGiUtils`.

--- ru.aplix.ltk.osgi/src/ru/aplix/ltk/osgi/OSGiUtils.java
+++ ru.aplix.ltk.osgi/src/ru/aplix/ltk/osgi/OSGiUtils.java
@@ -5,13 +5,13 @@ import org.osgi.framework.BundleContext;
 import ru.aplix.ltk.core.util.Parameters;
 
 
-public final class OSGIUtils {
+public final class OSGiUtils {
 
 	public static Parameters bundleParameters(BundleContext context) {
 		return new Parameters(new BundleContextParameters(context));
 	}
 
-	private OSGIUtils() {
+	private OSGiUtils() {
 	}
 
 }

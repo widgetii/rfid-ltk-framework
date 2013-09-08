15c29ddaca7233c5c277d5597c6d083fe852d031
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2013-09-08 15:29:42 +0700

    Collector HTTP: Client request contains URL now instead of simple string.

--- ru.aplix.ltk.collector.http/src/ru/aplix/ltk/collector/http/ClrClientRequest.java
+++ ru.aplix.ltk.collector.http/src/ru/aplix/ltk/collector/http/ClrClientRequest.java
@@ -1,33 +1,61 @@
 package ru.aplix.ltk.collector.http;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Objects;
+
 import ru.aplix.ltk.core.util.Parameterized;
 import ru.aplix.ltk.core.util.Parameters;
 
 
 public class ClrClientRequest implements Parameterized {
 
-	private String clientURL;
+	private URL clientURL;
 
 	public ClrClientRequest(Parameters params) {
 		read(params);
 	}
 
-	public String getClientURL() {
+	public URL getClientURL() {
 		return this.clientURL;
 	}
 
-	public void setClientURL(String clientURL) {
+	public void setClientURL(URL clientURL) {
+		if (clientURL != null) {
+
+			final String protocol = clientURL.getProtocol();
+
+			if (!"http".equals(protocol) && !"https".equals(protocol)) {
+				throw new IllegalArgumentException(
+						"HTTP/HTTPS client URL expected: " + clientURL);
+			}
+		}
+
 		this.clientURL = clientURL;
 	}
 
 	@Override
 	public void read(Parameters params) {
-		setClientURL(params.valueOf("clientURL"));
+		setClientURL(clientURLFromParams(params));
 	}
 
 	@Override
 	public void write(Parameters params) {
 		params.set("clientURL", getClientURL());
+	}
+
+	private URL clientURLFromParams(Parameters params) {
+		try {
+
+			final String url = params.valueOf(
+					"clientURL",
+					null,
+					Objects.toString(getClientURL(), null));
+
+			return url != null ? new URL(url) : null;
+		} catch (MalformedURLException e) {
+			throw new IllegalArgumentException("Invalid client URL", e);
+		}
 	}
 
 }

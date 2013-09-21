3a1294f640cc96d9af126fe6c452ad36b45c20fb
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2013-09-21 19:37:43 +0700

    Ctg UI: Add exclusive mode checkbox and antennas filter option.

--- ru.aplix.ltk.driver.ctg.ui/src/ru/aplix/ltk/driver/ctg/ui/CtgRfSettingsPanel.java
+++ ru.aplix.ltk.driver.ctg.ui/src/ru/aplix/ltk/driver/ctg/ui/CtgRfSettingsPanel.java
@@ -2,6 +2,7 @@ package ru.aplix.ltk.driver.ctg.ui;
 
 import static ru.aplix.ltk.core.collector.DefaultRfTrackingPolicy.RF_INVALIDATION_TIMEOUT;
 import static ru.aplix.ltk.core.collector.DefaultRfTrackingPolicy.RF_TRANSACTION_TIMEOUT;
+import static ru.aplix.ltk.core.util.IntSet.parseIntSet;
 import static ru.aplix.ltk.driver.ctg.CtgRfSettings.*;
 
 import java.awt.GridBagConstraints;
@@ -10,6 +11,7 @@ import java.awt.Insets;
 
 import javax.swing.*;
 
+import ru.aplix.ltk.core.util.IntSet;
 import ru.aplix.ltk.driver.ctg.CtgRfSettings;
 
 
@@ -22,6 +24,9 @@ public class CtgRfSettingsPanel extends JPanel {
 	};
 	private static final String[] RO_SPEC_IDS = {
 		CTG_RF_ROSPEC_ID.getDefault().toString()
+	};
+	private static final String[] ANTENNAS = {
+		CTG_RF_ANTENNAS.getDefault().toString()
 	};
 	private static final String[] CONNECTION_TIMEOUTS = {
 		Long.toString(CTG_RF_CONNECTION_TIMEOUT.getDefault() / 2),
@@ -59,6 +64,8 @@ public class CtgRfSettingsPanel extends JPanel {
 	private final JComboBox<String> readerHost;
 	private final JComboBox<String> readerPort;
 	private final JComboBox<String> roSpecId;
+	private final JCheckBox exclusiveMode;
+	private final JComboBox<String> antennas;
 	private final JComboBox<String> connectionTimeout;
 	private final JComboBox<String> transactionTimeout;
 	private final JComboBox<String> reconnectionDelay;
@@ -78,6 +85,9 @@ public class CtgRfSettingsPanel extends JPanel {
 		this.readerPort.setEditable(true);
 		this.roSpecId = new JComboBox<>(RO_SPEC_IDS);
 		this.roSpecId.setEditable(true);
+		this.exclusiveMode = new JCheckBox("Монопольно");
+		this.antennas = new JComboBox<>(ANTENNAS);
+		this.antennas.setEditable(true);
 		this.connectionTimeout = new JComboBox<>(CONNECTION_TIMEOUTS);
 		this.connectionTimeout.setEditable(true);
 		this.transactionTimeout = new JComboBox<>(TRANSACTION_TIMEOUTS);
@@ -94,7 +104,11 @@ public class CtgRfSettingsPanel extends JPanel {
 
 		addOption("Адрес ридера", this.readerHost);
 		addOption("Порт ридера", this.readerPort);
-		addOption("Идентификатор клиента (число)", this.roSpecId);
+		addOption(
+				"Идентификатор клиента (число)",
+				this.roSpecId,
+				this.exclusiveMode);
+		addOption("Антенны", this.antennas);
 		addOption("Время ожидания соединения, мс", this.connectionTimeout);
 		addOption("Время ожидания ответа, мс", this.transactionTimeout);
 		addOption("Задержка переподключения, мс", this.reconnectionDelay);
@@ -126,6 +140,10 @@ public class CtgRfSettingsPanel extends JPanel {
 		settings.setROSpecId(intOption(
 				this.roSpecId,
 				CTG_RF_ROSPEC_ID.getDefault()));
+		settings.setExclusiveMode(this.exclusiveMode.isSelected());
+		settings.setAntennas(intSetOption(
+				this.antennas,
+				settings.getAntennas()));
 		settings.setConnectionTimeout(longOption(
 				this.connectionTimeout,
 				CTG_RF_CONNECTION_TIMEOUT.getDefault()));
@@ -159,6 +177,10 @@ public class CtgRfSettingsPanel extends JPanel {
 				Integer.toString(settings.getReaderPort()));
 		this.roSpecId.setSelectedItem(
 				Integer.toString(settings.getROSpecId()));
+		this.exclusiveMode.setSelected(
+				settings.isExclusiveMode());
+		this.antennas.setSelectedItem(
+				settings.getAntennas().toString());
 		this.connectionTimeout.setSelectedItem(
 				Long.toString(settings.getConnectionTimeout()));
 		this.transactionTimeout.setSelectedItem(
@@ -177,6 +199,13 @@ public class CtgRfSettingsPanel extends JPanel {
 	}
 
 	private void addOption(String label, JComponent component) {
+		addOption(label, component, null);
+	}
+
+	private void addOption(
+			String label,
+			JComponent component,
+			JComponent extra) {
 
 		final GridBagConstraints labelConstraints = new GridBagConstraints();
 
@@ -195,9 +224,24 @@ public class CtgRfSettingsPanel extends JPanel {
 		constraints.gridx = 1;
 		constraints.gridy = this.line;
 		constraints.weightx = 1;
-		constraints.insets = new Insets(2, 2, 2, 10);
+		constraints.insets = new Insets(2, 2, 2, extra != null ? 2 : 10);
 
 		add(component, constraints);
+
+		if (extra != null) {
+
+			final GridBagConstraints extraConstraints =
+					new GridBagConstraints();
+
+			extraConstraints.anchor = GridBagConstraints.WEST;
+			extraConstraints.fill = GridBagConstraints.NONE;
+			extraConstraints.gridx = 2;
+			extraConstraints.gridy = this.line;
+			extraConstraints.weightx = 0.3;
+			extraConstraints.insets = new Insets(2, 2, 2, 10);
+
+			add(extra, extraConstraints);
+		}
 
 		++this.line;
 	}
@@ -233,6 +277,24 @@ public class CtgRfSettingsPanel extends JPanel {
 		}
 
 		comboBox.setSelectedItem(Long.toString(defaultValue));
+
+		return defaultValue;
+	}
+
+	private static IntSet intSetOption(
+			JComboBox<?> comboBox,
+			IntSet defaultValue) {
+
+		final String selected = (String) comboBox.getSelectedItem();
+
+		if (selected != null) {
+			try {
+				return parseIntSet(selected);
+			} catch (IllegalArgumentException e) {
+			}
+		}
+
+		comboBox.setSelectedItem(defaultValue.toString());
 
 		return defaultValue;
 	}

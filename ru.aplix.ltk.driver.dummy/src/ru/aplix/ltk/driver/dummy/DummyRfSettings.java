da73dceec3f477db0cdb12c11bdfa76d7c45a37f
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2013-09-21 15:11:00 +0700

    Dummy: Convert `DummyRfSettings` to use new parameters API.

--- ru.aplix.ltk.driver.dummy/src/ru/aplix/ltk/driver/dummy/DummyRfSettings.java
+++ ru.aplix.ltk.driver.dummy/src/ru/aplix/ltk/driver/dummy/DummyRfSettings.java
@@ -1,25 +1,39 @@
 package ru.aplix.ltk.driver.dummy;
 
+import static ru.aplix.ltk.core.util.NumericParameterType.INTEGER_PARAMETER_TYPE;
+import static ru.aplix.ltk.core.util.NumericParameterType.LONG_PARAMETER_TYPE;
+import static ru.aplix.ltk.core.util.ParameterType.STRING_PARAMETER_TYPE;
 import ru.aplix.ltk.core.RfSettings;
 import ru.aplix.ltk.core.collector.RfTrackingPolicy;
+import ru.aplix.ltk.core.util.Parameter;
 import ru.aplix.ltk.core.util.Parameters;
 import ru.aplix.ltk.driver.dummy.impl.DummyTrackingPolicy;
 
 
 public class DummyRfSettings implements RfSettings, Cloneable {
 
-	public static final long DUMMY_DEFAULT_GENERATION_PERIOD = 5000L;
-	public static final long DUMMY_DEFAULT_PRESENCE_DURATION = 3000L;
-	public static final long DUMMY_DEFAULT_READ_PERIOD = 1000L;
-	public static final int DUMMY_DEFAULT_MAX_TAGS = 5;
+	public static final Parameter<String> DUMMY_DEVICE_ID =
+			STRING_PARAMETER_TYPE.parameter("deviceId")
+			.byDefault("TEST Reader");
+	public static final Parameter<Long> DUMMY_GENERATION_PERIOD =
+			LONG_PARAMETER_TYPE.parameter("generationPeriod")
+			.byDefault(5000L);
+	public static final Parameter<Long> DUMMY_PRESENCE_DURATION =
+			LONG_PARAMETER_TYPE.parameter("presenceDuration")
+			.byDefault(3000L);
+	public static final Parameter<Long> DUMMY_READ_PERIOD =
+			LONG_PARAMETER_TYPE.parameter("readPeriod")
+			.byDefault(1000L);
+	public static final Parameter<Integer> DUMMY_MAX_TAGS =
+			INTEGER_PARAMETER_TYPE.parameter("maxTags").byDefault(5);
 
 	private RfTrackingPolicy trackingPolicy = new DummyTrackingPolicy(this);
 	private String portId = "TEST Port";
-	private String deviceId = "TEST Reader";
-	private long generationPeriod = DUMMY_DEFAULT_GENERATION_PERIOD;
-	private long presenceDuration = DUMMY_DEFAULT_PRESENCE_DURATION;
-	private long readPeriod = DUMMY_DEFAULT_READ_PERIOD;
-	private int maxTags = DUMMY_DEFAULT_MAX_TAGS;
+	private String deviceId = DUMMY_DEVICE_ID.getDefault();
+	private long generationPeriod = DUMMY_GENERATION_PERIOD.getDefault();
+	private long presenceDuration = DUMMY_PRESENCE_DURATION.getDefault();
+	private long readPeriod = DUMMY_READ_PERIOD.getDefault();
+	private int maxTags = DUMMY_MAX_TAGS.getDefault();
 
 	@Override
 	public RfTrackingPolicy getTrackingPolicy() {
@@ -63,7 +77,7 @@ public class DummyRfSettings implements RfSettings, Cloneable {
 
 	public void setPresenceDuration(long duration) {
 		this.presenceDuration =
-				duration > 0 ? duration : DUMMY_DEFAULT_PRESENCE_DURATION;
+				duration > 0 ? duration : DUMMY_PRESENCE_DURATION.getDefault();
 	}
 
 	public long getReadPeriod() {
@@ -71,7 +85,7 @@ public class DummyRfSettings implements RfSettings, Cloneable {
 	}
 
 	public void setReadPeriod(long period) {
-		this.readPeriod = period > 0 ? period : DUMMY_DEFAULT_READ_PERIOD;
+		this.readPeriod = period > 0 ? period : DUMMY_READ_PERIOD.getDefault();
 	}
 
 	public int getMaxTags() {
@@ -79,37 +93,35 @@ public class DummyRfSettings implements RfSettings, Cloneable {
 	}
 
 	public void setMaxTags(int maxTags) {
-		this.maxTags = maxTags > 0 ? maxTags : DUMMY_DEFAULT_MAX_TAGS;
+		this.maxTags = maxTags > 0 ? maxTags : DUMMY_MAX_TAGS.getDefault();
 	}
 
 	@Override
 	public void read(Parameters params) {
-		setDeviceId(params.valueOf("deviceId", getDeviceId()));
-		setGenerationPeriod(params.longValueOf(
-				"generationPeriod",
-				DUMMY_DEFAULT_GENERATION_PERIOD,
+		setDeviceId(params.valueOf(
+				DUMMY_DEVICE_ID,
+				getDeviceId()));
+		setGenerationPeriod(params.valueOf(
+				DUMMY_GENERATION_PERIOD,
 				getGenerationPeriod()));
-		setPresenceDuration(params.longValueOf(
-				"presenceDuration",
-				DUMMY_DEFAULT_PRESENCE_DURATION,
+		setPresenceDuration(params.valueOf(
+				DUMMY_PRESENCE_DURATION,
 				getPresenceDuration()));
-		setReadPeriod(params.longValueOf(
-				"readPeriod",
-				DUMMY_DEFAULT_READ_PERIOD,
+		setReadPeriod(params.valueOf(
+				DUMMY_READ_PERIOD,
 				getReadPeriod()));
-		setMaxTags(params.intValueOf(
-				"maxTags",
-				DUMMY_DEFAULT_MAX_TAGS,
+		setMaxTags(params.valueOf(
+				DUMMY_MAX_TAGS,
 				getMaxTags()));
 	}
 
 	@Override
 	public void write(Parameters params) {
-		params.set("deviceId", getDeviceId())
-		.set("generationPeriod", getGenerationPeriod())
-		.set("presenceDuration", getPresenceDuration())
-		.set("readPeriod", getReadPeriod())
-		.set("maxTags", getMaxTags());
+		params.set(DUMMY_DEVICE_ID, getDeviceId())
+		.set(DUMMY_GENERATION_PERIOD, getGenerationPeriod())
+		.set(DUMMY_PRESENCE_DURATION, getPresenceDuration())
+		.set(DUMMY_READ_PERIOD, getReadPeriod())
+		.set(DUMMY_MAX_TAGS, getMaxTags());
 	}
 
 	@Override

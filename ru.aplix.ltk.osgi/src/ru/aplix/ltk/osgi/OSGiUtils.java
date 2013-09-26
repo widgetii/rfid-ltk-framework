751602237f7b75220f336b3c177e1745417e387d
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2013-09-26 14:12:35 +0700

    OSGi: Document `OSGiUtils` class.

--- ru.aplix.ltk.osgi/src/ru/aplix/ltk/osgi/OSGiUtils.java
+++ ru.aplix.ltk.osgi/src/ru/aplix/ltk/osgi/OSGiUtils.java
@@ -5,8 +5,21 @@ import org.osgi.framework.BundleContext;
 import ru.aplix.ltk.core.util.Parameters;
 
 
+/**
+ * Various OSGi utilities.
+ */
 public final class OSGiUtils {
 
+	/**
+	 * Creates unmodifiable parameters with values stored in bundle context.
+	 *
+	 * <p>The parameters instance created can not be iterated over, and thus
+	 * does not support serialization.</p>
+	 *
+	 * @param context bundle context to get values from
+	 *
+	 * @return new parameters instance.
+	 */
 	public static Parameters bundleParameters(BundleContext context) {
 		return new Parameters(new BundleContextParameters(context));
 	}

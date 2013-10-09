fe187a2eef9ba7769d6fb5014c3feaace6cd6a9a
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2013-10-09 16:20:13 +0700

    Blackbox Driver: Create a singleton collector instead of collector per connection. Make manual message sending work.

--- ru.aplix.ltk.driver.blackbox/src/ru/aplix/ltk/driver/blackbox/impl/BlackboxRfConnection.java
+++ ru.aplix.ltk.driver.blackbox/src/ru/aplix/ltk/driver/blackbox/impl/BlackboxRfConnection.java
@@ -1,37 +1,29 @@
 package ru.aplix.ltk.driver.blackbox.impl;
 
+import static ru.aplix.ltk.core.source.RfSource.EMPTY_RF_SOURCE;
 import ru.aplix.ltk.core.RfConnection;
-import ru.aplix.ltk.core.source.RfDataReceiver;
+import ru.aplix.ltk.core.collector.RfCollector;
 import ru.aplix.ltk.core.source.RfSource;
-import ru.aplix.ltk.core.source.RfStatusUpdater;
 import ru.aplix.ltk.driver.blackbox.BlackboxRfSettings;
 
 
-final class BlackboxRfConnection extends RfConnection implements RfSource {
+final class BlackboxRfConnection extends RfConnection {
 
-	public BlackboxRfConnection(BlackboxRfSettings settings) {
+	private final RfCollector collector;
+
+	BlackboxRfConnection(RfCollector collector, BlackboxRfSettings settings) {
 		super(settings);
+		this.collector = collector;
 	}
 
 	@Override
 	protected RfSource createSource() {
-		return this;
+		return EMPTY_RF_SOURCE;
 	}
 
 	@Override
-	public void requestRfStatus(RfStatusUpdater updater) {
-	}
-
-	@Override
-	public void requestRfData(RfDataReceiver receiver) {
-	}
-
-	@Override
-	public void rejectRfData() {
-	}
-
-	@Override
-	public void rejectRfStatus() {
+	protected RfCollector createCollector() {
+		return this.collector;
 	}
 
 }
--- ru.aplix.ltk.driver.blackbox/src/ru/aplix/ltk/driver/blackbox/impl/BlackboxRfProvider.java
+++ ru.aplix.ltk.driver.blackbox/src/ru/aplix/ltk/driver/blackbox/impl/BlackboxRfProvider.java
@@ -1,11 +1,25 @@
 package ru.aplix.ltk.driver.blackbox.impl;
 
+import java.util.LinkedList;
+
 import ru.aplix.ltk.core.RfConnection;
 import ru.aplix.ltk.core.RfProvider;
+import ru.aplix.ltk.core.collector.RfCollector;
+import ru.aplix.ltk.core.collector.RfTracker;
+import ru.aplix.ltk.core.collector.RfTracking;
+import ru.aplix.ltk.core.source.RfDataMessage;
+import ru.aplix.ltk.core.source.RfSource;
 import ru.aplix.ltk.driver.blackbox.BlackboxRfSettings;
 
 
-class BlackboxRfProvider implements RfProvider<BlackboxRfSettings> {
+class BlackboxRfProvider
+		implements RfProvider<BlackboxRfSettings>, RfTracker {
+
+	private final RfCollector collector =
+			new RfCollector(RfSource.EMPTY_RF_SOURCE, this);
+	private final LinkedList<RfTracker> trackers = new LinkedList<>();
+	private RfTracking tracking;
+	private boolean trackingStarted;
 
 	@Override
 	public String getId() {
@@ -34,7 +48,52 @@ class BlackboxRfProvider implements RfProvider<BlackboxRfSettings> {
 
 	@Override
 	public RfConnection connect(BlackboxRfSettings settings) {
-		return new BlackboxRfConnection(settings.clone());
+
+		final RfTracker tracker = settings.getTracker();
+
+		if (tracker != null) {
+			addTracker(tracker);
+		}
+
+		return new BlackboxRfConnection(this.collector, settings);
+	}
+
+	@Override
+	public synchronized void initRfTracker(RfTracking tracking) {
+		this.tracking = tracking;
+		for (RfTracker tracker : this.trackers) {
+			tracker.initRfTracker(tracking);
+		}
+	}
+
+	@Override
+	public synchronized void startRfTracking() {
+		this.trackingStarted = true;
+		for (RfTracker tracker : this.trackers) {
+			tracker.startRfTracking();
+		}
+	}
+
+	@Override
+	public void rfData(RfDataMessage dataMessage) {
+	}
+
+	@Override
+	public synchronized void stopRfTracking() {
+		this.trackingStarted = false;
+		for (RfTracker tracker : this.trackers) {
+			tracker.startRfTracking();
+		}
+	}
+
+	private synchronized void addTracker(RfTracker tracker) {
+		this.trackers.add(tracker);
+		if (this.tracking != null) {
+			tracker.initRfTracker(this.tracking);
+			if (this.trackingStarted) {
+				tracker.startRfTracking();
+			}
+		}
 	}
 
 }

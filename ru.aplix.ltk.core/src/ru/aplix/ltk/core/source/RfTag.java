cf0e99c0a6d10a7bb9760cc08dcbdbfee877ef58
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2013-10-10 17:53:08 +0700

    Core: Allow to restore `RfTag` from its string representation.

--- ru.aplix.ltk.core/src/ru/aplix/ltk/core/source/RfTag.java
+++ ru.aplix.ltk.core/src/ru/aplix/ltk/core/source/RfTag.java
@@ -13,13 +13,24 @@ public final class RfTag {
 	private final byte[] data;
 
 	/**
-	 * Constructs RFID tag..
+	 * Constructs RFID tag.
 	 *
 	 * @param data binary tag data.
 	 */
 	public RfTag(byte[] data) {
 		requireNonNull(data, "RFID tag data not specified");
 		this.data = data;
+	}
+
+	/**
+	 * Reconstructs RFID tag from its string representation.
+	 *
+	 * @param string a tag string representation created by
+	 * {@link #toHexString()}, or {@link #toString()}.
+	 */
+	public RfTag(String string) {
+		requireNonNull(string, "RFID tag string not specified");
+		this.data = parseData(string);
 	}
 
 	/**
@@ -96,6 +107,35 @@ public final class RfTag {
 		toHexString(out);
 
 		return out.toString();
+	}
+
+	private static byte[] parseData(String string) {
+
+		final String str = removePrefix(string);
+		final int len = str.length();
+		final byte[] data = new byte[(len + 1) >> 1];
+
+		for (int i = 0; i < len; i += 2) {
+
+			final int val = Integer.parseInt(
+					str.substring(i, Math.min(len, i + 2)),
+					16);
+
+			data[i >> 1] = (byte) val;
+		}
+
+		return data;
+	}
+
+	private static String removePrefix(String string) {
+		final String str;
+
+		if (string.startsWith("0x")) {
+			str = string.substring(2);
+		} else {
+			str = string;
+		}
+		return str;
 	}
 
 }

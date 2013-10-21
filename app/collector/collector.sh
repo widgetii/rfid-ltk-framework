10ecd945be3237fcde50bde9121ecd05794d73bf
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2013-10-21 17:59:50 +0700

    Collector App: Fix launcher script.

--- app/collector/collector.sh
+++ app/collector/collector.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd target && \
-  java -Dlog4.configuration=file:configuration/log4j.xml \
+  java -Dlog4j.configuration=file:configuration/log4j.xml \
   -Dlogback.configuration=configuration/logback.xml \
   -jar $(find plugins -name 'org.eclipse.equinox.launcher_*.jar') "$@"

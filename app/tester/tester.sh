05aa0bf5b52eca4ba2a531ce58c70dd068e0ac0b
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2013-10-24 14:57:42 +0700

    Tester App: Enable logging.

--- app/tester/tester.sh
+++ app/tester/tester.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-cd target && java -jar $(find plugins -name 'org.eclipse.equinox.launcher_*.jar') "$@"
+cd target && \
+  java -Dlog4j.configuration=file:configuration/log4j.xml \
+  -jar $(find plugins -name 'org.eclipse.equinox.launcher_*.jar') "$@"

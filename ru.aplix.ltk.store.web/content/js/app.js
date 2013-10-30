8fcee4c086b3a76225b615f14316239ddad80518
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2013-10-30 17:59:18 +0700

    Store Web: Add tag reporting page skeleton.

--- ru.aplix.ltk.store.web/content/js/app.js
+++ ru.aplix.ltk.store.web/content/js/app.js
@@ -4,17 +4,16 @@ angular.module(
 			"ngResource",
 			"ui.bootstrap",
 			"notifier",
-			"rfid-tag-store.receivers"
+			"rfid-tag-store.receivers",
+			"rfid-tag-store.tags",
 		])
 .config(function($locationProvider) {
 	$locationProvider.hashPrefix('!');
 })
 .config(function($routeProvider) {
-	$routeProvider.when(
-			'/receivers',
-			{
-				templateUrl: 'receivers/receivers.html'
-			})
+	$routeProvider
+	.when('/receivers', {templateUrl: 'receivers/receivers.html'})
+	.when('/tags', {templateUrl: 'tags/tags.html'})
 	.otherwise({redirectTo: '/receivers'});
 })
 .controller("NavCtrl", function($scope, $location) {

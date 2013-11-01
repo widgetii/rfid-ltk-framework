1ba992b471844b1c0bdb2f49ba3b8a8908add793
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2013-11-01 14:11:08 +0700

    Store Web: Tag search URLs.

--- ru.aplix.ltk.store.web/content/tags/tags.js
+++ ru.aplix.ltk.store.web/content/tags/tags.js
@@ -1,19 +1,49 @@
 angular.module('rfid-tag-store.tags', ["notifier"])
-.controller("RfTagsCtrl", function($scope, $http, $notifier) {
-	$scope.query = {};
-	$scope.state = {
+.controller(
+		"RfTagsCtrl",
+		function(
+				$scope,
+				$http,
+				$notifier,
+				$routeParams,
+				$location) {
+	var state = {
 		inProgress: false
 	};
-	$scope.tags = {totalCount: 0, events: []};
+	var query = {
+		receiver: $routeParams.receiver != "all" ? $routeParams.receiver : null,
+		tag: $routeParams.tag,
+		since: $routeParams.since,
+		offset: $routeParams.offset,
+		limit: $routeParams.limit
+	};
+	var tags = {
+		totalCount: 0,
+		events: []
+	};
+
+	$scope.state = state;
+	$scope.query = query;
+	$scope.tags = tags;
+
 	$scope.receiverName = function(receiver) {
 		if (!receiver.remoteURL) return "#" + receiver.id;
 		return receiver.remoteURL + " (#" + receiver.id + ")";
 	};
+
 	$scope.search = function() {
-		var state = $scope.state;
 		if (state.inProgress) return;
-		var query = $scope.query;
-		if (!query.limit) query.limit = 50;
+		state.inProgress = true;
+		var receiver = query.receiver ? query.receiver : "all";
+		var s = {};
+		if (query.tag) s.tag = query.tag;
+		if (query.since) s.since = query.since;
+		if (query.offset) s.offset = query.offset;
+		if (query.limit) s.limit = query.limit;
+		$location.path("/tags/" + receiver).search(s);
+	};
+
+	function find() {
 		var loading = $notifier.info("Загрузка...");
 		state.inProgress = true;
 		function done() {
@@ -23,7 +53,7 @@ angular.module('rfid-tag-store.tags', ["notifier"])
 		$http.post("tags/find.json", query, {requestType: "json"})
 		.success(function(data, status) {
 			done();
-			$scope.tags = data;
+			angular.copy(data, tags);
 		})
 		.error(function(data, status) {
 			done();
@@ -31,5 +61,7 @@ angular.module('rfid-tag-store.tags', ["notifier"])
 					"Не удалось найти теги",
 					"ОШИБКА " + status);
 		});
-	};
+	}
+
+	if ($routeParams.receiver) find();
 });
